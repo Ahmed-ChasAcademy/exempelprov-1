3f3424771472500121161ea1dfa520488f7dfaae
Ahmed <Ahmed.Bahobeshi@chasacademy.se>
2024-12-04 20:39:48 +0100

    trying question 2

--- Question 2/question2.js
+++ Question 2/question2.js
@@ -1,12 +1,62 @@
-// 2.1 Hämta todo-data från denna url: https://jsonplaceholder.typicode.com/todos/
-// Skriv ut med html på skärmen titeln på alla todo-objekt som har
-// userId: 1
+
+const doUndoBtn = () => {
+  if(todobtn.textContent === "do") {
+    todobtn.textContent = "undo"
+  } else if(todobtn.textContent === "undo") {
+          todobtn.textContent = "do"
+    }
+};
+
 
 async function getTodos() {
-    // Din kod här
+  try {
+    
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos/");
+    
+    const data = await response.json();
+    
+
+    console.log(data);
+
+    
+    const toDoList = document.getElementById('toDoList');
+    
+    
+    toDoList.innerHTML = '';
+
+    
+    data.forEach(todo => {
+      const todoItem = document.createElement('li');
+      const todobtn = document.createElement("button"); 
+      const hrLine = document.createElement("hr")
+      todobtn.textContent = "do";
+      todoItem.textContent = `${todo.id}: ${todo.title} - ${todo.completed ? 'Completed' : 'Not completed'}`;
+      toDoList.appendChild(todoItem); 
+      toDoList.appendChild(hrLine)
+      todoItem.appendChild(todobtn);
+      todobtn.addEventListener("click", doUndoBtn);
+
+      
+    });
+  } catch (error) {
+    console.error('Error fetching data:', error);
   }
-  
-  getTodos();
+}
+
+getTodos(); 
+
+
+
+
+
+
+
+
+
+
+
+// Call the function to fetch and display the todos
+
   
   // 2.2 Lägg till en done-knapp under varje titel. Om todo-objektet är genomfört,
   // alltså completed: true, så ska det stå "Undo" på knappen annars "Done"
@@ -15,3 +65,8 @@ async function getTodos() {
   // Om det står "Undo" på knappen ska titeln vara genomstruken, alltså i css:
   // text-decoration: line-through;
   // Annars ska den inte vara det.
+
+
+  // 2.1 Hämta todo-data från denna url: https://jsonplaceholder.typicode.com/todos/
+// Skriv ut med html på skärmen titeln på alla todo-objekt som har
+// userId: 1
